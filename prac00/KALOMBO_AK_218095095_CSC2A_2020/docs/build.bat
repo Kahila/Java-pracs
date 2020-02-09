b2ff170df48a7c2fcf2db217e3ee1cf07a930889
Adonis Kahila <46842993+Kahila@users.noreply.github.com>
2020-02-09 07:37:32 -0800

    Add files via upload

--- prac00/KALOMBO_AK_218095095_CSC2A_2020/docs/build.bat
+++ prac00/KALOMBO_AK_218095095_CSC2A_2020/docs/build.bat
@@ -1,12 +1,11 @@
 @ECHO OFF
-set PATH=%PATH%;C:\Program Files (x86)\Java\jdk1.8.0_241\bin\
+set PATH=%PATH%;C:\Program Files\Java\jdk-10.0.1\bin
 
 cd ..
 
 set PRAC_SRC=./src
 set PRAC_BIN=./bin
-set PRAC_DOCS=./bin
 
-javac -sourcepath %PRAC_SRC% -cp %PRAC_BIN% -d %PRAC_BIN%\Main.java
+javac -sourcepath %PRAC_SRC% -cp %PRAC_BIN% -d %PRAC_BIN% %PRAC_SRC%/Main.java
 java -cp %PRAC_BIN% Main
 pause
